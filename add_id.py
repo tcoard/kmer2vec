19a25a771a1eeda215827e954cd54e144c3d3be3
tcoard <thomas.coard@gmail.com>
2020-09-07 23:02:14 -0400

    finished blast comparer. still need to polish some things and run more tests

--- add_id.py
+++ add_id.py
@@ -1,22 +1,37 @@
 import json
-num_scores_to_save = 6
-ending_json = list()
-with open("formatted.json") as f:
-    data = json.load(f)
-    for seq in data:
-        filtered_seqs = sorted(seq["top_100_scores"], key=lambda k: k["score"], reverse=True)[0:num_scores_to_save+1]
-        for filtered_seq in filtered_seqs:
-            with open("exp_protein_sequences.fasta") as fasta:
-                prev_line = "" 
-                for line in fasta:
-                    line = line.strip()
-                    if line == filtered_seq["seq"]:
-                        break
-                    prev_line = line
-                else:
-                    raise ValueError(f"could not find {filtered_seq['seq']}")
-                filtered_seq["id"] = prev_line.replace('>', '')
 
-        ending_json.append(filtered_seqs)
-    print(json.dumps(ending_json))
 
+def find_id(seq):
+    with open("exp_protein_sequences.fasta") as fasta:
+        prev_line = ""
+        for line in fasta:
+            line = line.strip()
+            if line == seq:
+                break
+            prev_line = line
+        else:
+            raise ValueError(f"could not find {seq}")
+
+        return prev_line.replace(">", "")
+
+
+def main():
+    num_scores_to_save = 6
+    ending_json = list()
+    with open("formatted.json") as f:
+        data = json.load(f)
+        for seq in data:
+            filtered_seqs = sorted(
+                seq["top_100_scores"], key=lambda k: k["score"], reverse=True
+            )[0 : num_scores_to_save + 1]
+            for filtered_seq in filtered_seqs:
+                filtered_seq["id"] = find_id(filtered_seq["seq"])
+
+            ending_json.append(
+                {"original_id": find_id(seq["original_seq"]), "matches": filtered_seqs}
+            )
+        print(json.dumps(ending_json))
+
+
+if __name__ == "__main__":
+    main()
--- compare_with_blast.py
+++ compare_with_blast.py
@@ -1,14 +1,40 @@
 import os
 import json
 
-fname = "13.out"
-if not os.path.exists("{fname}_1.json"):
-    os.system(f"blastp -db exp_protein_sequences.fasta -query Q64331.fsa -out {fname} -outfmt 13")
-with open("shortened_formatted.json") as w2v_file:
-    for 
-with open(f"{fname}_1.json", 'r') as f:
-    data = json.load(f)
-    hits = data["BlastOutput2"]["report"]["results"]["search"]["hits"]
-    blast_matches = [hit["description"][0]["accession"] for hit in hits]
 
-    
+def main():
+    # TODO put in check(s) to make sure that I am actually evaluating the top scores from both
+    num_to_save = 10
+    final = dict()
+    with open("blast_compare_ready.json") as our_match_file:
+        data = json.load(our_match_file)
+        for seq in data:
+            seq_id = seq["original_id"]
+            final[seq_id] = dict()
+            fname = f"{seq_id}.out"
+            if not os.path.exists(f"{fname}_1.json"):
+                os.system(
+                    f"blastp -db exp_protein_sequences.fasta -query Q64331.fsa -out {fname} -outfmt 13"
+                )
+            with open(f"{fname}_1.json", "r") as f:
+                data = json.load(f)
+                hits = data["BlastOutput2"]["report"]["results"]["search"]["hits"]
+                final[seq_id]["blast_matches"] = [
+                    hit["description"][0]["accession"] for hit in hits
+                ][0 : num_to_save + 1]
+                final[seq_id]["our_matches"] = []
+                final[seq_id]["in_both"] = []
+                for i, match in enumerate(seq["matches"]):
+                    our_id = match["id"]
+                    final[seq_id]["our_matches"].append(our_id)
+                    if our_id in final[seq_id]["blast_matches"]:
+                        final[seq_id]["in_both"].append(our_id)
+                    # TODO there is a better way to do this
+                    if i == 9:
+                        break
+
+        print(json.dumps(final))
+
+
+if __name__ == "__main__":
+    main()
